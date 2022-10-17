27cf9c5ed877921997ef4ca6c886447505747ea4
davi38 <davi4alves@gmail.com>
2022-10-16 22:42:47 -0300

    feat: db config and structure

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const colors = require('colors')
 const app = express();
+dotenv.config();
+
+const connectDB = require("./config/db");
+
+
+app.use(cors());
 
 app.get("", (req, res) => {
-  res.status(200).json({ message: "Server online" });
+  res
+    .status(200)
+    .json({ message: "Server online", uri: process.env.MONGO_URI });
 });
 
 const PORT = process.env.PORT || 3001;
 
+connectDB();
+
 app.listen(PORT, () => {
-  console.log(`Server online on port ${PORT}`);
+  console.log(`Server online on port ${PORT}`.brightBlue.bold);
 });
